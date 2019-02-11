48a9e7220348bf80ba9c6beb727455c3f50757b5
Nicholas Alexi <nlas.alexi@gmail.com>
2019-02-11 14:48:02 -0200

    Adicionar arquivos de projeto.

---
+++ ProjetoEquiclass/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEquiclass
+{
+    public class Class1
+    {
+    }
+}
